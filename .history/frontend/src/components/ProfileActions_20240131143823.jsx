49b012425175bead5eb71bbc3523a2a8c352741a
bShariff <bilaal441@hotmail.com>
2024-01-31 16:46:43 +0000

    added some features

---
+++ .history/frontend/src/components/ProfileActions_20240131143823.jsx
@@ -0,0 +1,45 @@
+import {useState} from "react";
+// import classes from "./Profile.module.css";
+import Action from "./Action";
+import MyModal from "./Modal";
+import User from "./User";
+const ProfileActions = ({user, handleClose, handleShow, show}) => {
+  const [isActive, setIsActive] = useState("followers");
+  const toggleAction = (active) => {
+    setIsActive(active);
+  };
+
+  return (
+    <MyModal handleClose={handleClose} handleShow={handleShow} show={show}>
+      <ul>
+        <Action
+          numberAction={user.followerss.length}
+          actionName={"Followers"}
+          toggleAction={toggleAction}
+          active={isActive}
+        />
+        <Action
+          numberAction={user.fellowing.length}
+          actionName={"following"}
+          toggleAction={toggleAction}
+          active={isActive}
+        />
+      </ul>
+      <div>
+        <ul>
+          {user[isActive].map((user, i) => (
+            <li key={i}>
+              <User
+                userName={user.userName}
+                isLoggedIn={true}
+                name={user.name}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </MyModal>
+  );
+};
+
+export default ProfileActions;
